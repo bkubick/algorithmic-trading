fb1b679ddf13fab3a4d1c6ef89e099c2caf94406
bkubick <bjk2158@columbia.edu>
2023-10-17 19:56:04 -0500

    Add TickerType to types

--- algorithmic_trading/utils/types.py
+++ algorithmic_trading/utils/types.py
@@ -9,3 +9,4 @@ import numpy as np
 
 DateType = typing.Union[dt.datetime, dt.date]
 NumericType = typing.Union[int, float, np.int64, np.float64]
+TickerType: typing.NewType = typing.NewType('TickerType', str)
